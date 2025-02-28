3c470fa4a71fde6de2b87c38d25826aa4e40eada
BABO <136092946+BABO16@users.noreply.github.com>
2025-02-28 07:47:20 +0500

    Update changelog.js

--- sources/data/changelog.js
+++ sources/data/changelog.js
@@ -1,5 +1,5 @@
 const changelog = "Added sources for some new functions and nothing new.";
 const date = "Thursday, 27 February 2025";
-const version = "1.9";
+const version = "1.1";
 
 export { changelog, date, version };
